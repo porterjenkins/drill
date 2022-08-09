ed62528611e91a01367dc47d9abb5bf7e112e42a
Bryan Stringham <bryan.stringham@gmail.com>
2022-08-09 16:01:08 -0600

    Second commit.

--- Preprocessing/preprocessing_bs.py
+++ Preprocessing/preprocessing_bs.py
@@ -7,21 +7,33 @@ Created on Mon Jul  4 19:47:47 2022
 # %%
 import pandas as pd
 import numpy as np
+from os.path import exists
+import random
+# import array as arr
 # import datetime
 
-    # declare directory of the time collection period
-start_date_list = ['05-31']
-end_date_list   = ['06-02']
+# Get start and end date information
+dates = pd.read_csv('Dates.csv')
 
-# start_date_list = ['05-31']
-# end_date_list   = ['06-02']
+# Specify Train/Test/Validation Split
+train = 0.8
+test = 0.1
+val = 0.1
+test_increment = train+test
 
-for z in range(1):
+
+master_exists = exists('master_key.csv')
+if master_exists:
+    # Old Master Key filename used to determine whether a new segment needs to be created
+    old_master_key = pd.read_csv('master_key.csv',header=None)
 
 # %%
+dates_to_run = [14]
 
-    start_date =start_date_list[z]
-    end_date = end_date_list[z]
+for z in dates_to_run:
+
+    start_date = dates.iloc[z,1]
+    end_date   = dates.iloc[z,2]
     
     # open label data excel file
     label_dir = 'C:/Users/bryan/Documents/1 PhD/drill/Raw_Data/Data-2022-'+start_date+'_to_2022-'+end_date+'/label-data-2022-'+start_date+'_to_2022-'+end_date+'.xlsx'
@@ -41,12 +53,13 @@ for z in range(1):
     sensor_data = pd.read_csv(sensor_data_dir,header=None)
     sensor_data = sensor_data.values
     
-    num_users = month.size
+    num_users_total = month.size
+    num_users = np.nan_to_num(num_users,nan=1)
 
 # %%
     # get start and stop time indices
-    start_times_i = np.zeros(num_users,dtype=int)
-    stop_times_i = np.zeros(num_users,dtype=int)
+    start_times_i = np.zeros(num_users_total,dtype=int)
+    stop_times_i = np.zeros(num_users_total,dtype=int)
     n = len(sensor_data)
     a = 0
     b = 0
@@ -55,7 +68,7 @@ for z in range(1):
             stop_times_i[b] = i-1;
             b = b + 1
             
-        if(a>num_users-1):
+        if(a>num_users_total-1):
             break
         
         if(sensor_data[i,0]>start_times[a]):
@@ -64,25 +77,35 @@ for z in range(1):
 
 #%%
 
-    for j in range(num_users):
+    for j in range(num_users_total):
     # for j in range(1): # DEBUG line
         if label[j]<4 and duration[j]>0 and time_between[j]>0: #filter through bad users
             # Save key to the master user file key csv the...
-            # - Col 1: user ID (month, day, hour, minute, second of start time of pumping_user label (M) i.e. 03-01_13-22-58_1
-            # - Col 2: label (1, 2, or 3) 
-            # - Col 3: pumping month (MM)
-            # - Col 4: pumping day (DD)
-            # - Col 5: pumping start hour (HH)
-            # - Col 6: pumping start minute (MM)
-            # - Col 7: pumping start second (SS)
-            # - Col 8: pumping duration (Seconds)
-            # - Col 9: number of users that pumped during this time segment
+            # - Col 0: user ID (month, day, hour, minute, second of start time of pumping_user label (M) i.e. 03-01_13-22-58_1
+            # - Col 1: label (1, 2, or 3) 
+            # - Col 2: pumping month (MM)
+            # - Col 3: pumping day (DD)
+            # - Col 4: pumping start hour (HH)
+            # - Col 5: pumping start minute (MM)
+            # - Col 6: pumping start second (SS)
+            # - Col 7: pumping duration (Seconds)
+            # - Col 8: time between users (Seconds)
+            # - Col 9:number of users that pumped during this time segment
             # df.to_csv('master_key.csv',mode='a',index=False,header=False)
+            random_splitter = random.uniform(0, 1)
+            if random_splitter < train:
+                split_category = 1
+            elif random_splitter >= train and random_splitter < test_increment:
+                split_category = 2
+            elif random_splitter >= test_increment:
+                split_category = 3
+            
             user_ID_string = str(month[j])+'-'+str(day[j])+'_'+str(date_info[j,0])+'-'+str(date_info[j,1])+'-'+str(date_info[j,2])+'_'+str(label[j])
-            user_info = np.expand_dims(np.array([user_ID_string, label[j], month[j], day[j], date_info[j,0],date_info[j,1],date_info[j,2], duration[j], time_between[j]]),axis=0)
+            #                                    C0              C1        C2        C3      C4              C5              C6              C7           C8               C9            C10
+            user_info = np.expand_dims(np.array([user_ID_string, label[j], month[j], day[j], date_info[j,0], date_info[j,1], date_info[j,2], duration[j], time_between[j], num_users[j], split_category]),axis=0)
             user_info_df = pd.DataFrame(user_info)
-            if j == 0 and z==0:
-                user_info_df.to_csv('master_key.csv',index=False,header=False)
+            if j == 0 and z==dates_to_run[0]:
+                user_info_df.to_csv('master_key.csv',index=False,header=False,mode='w')
             else:
                 user_info_df.to_csv('master_key.csv',mode='a',index=False,header=False)
             # extract dt, HE1, HE2, and HE3 for that particular user
@@ -90,7 +113,24 @@ for z in range(1):
             sub_array = sensor_data[start_times_i[j]:stop_times_i[j],:]
             user_df = pd.DataFrame(sub_array)    
             
-            # save the pumping data to a csv in a folder
             string = 'Segments/'+user_ID_string+'.csv'
             user_df.to_csv(string,index=False,header=False)
+
+# Create train/test/val keys from master key
+master_key = pd.read_csv('master_key.csv',header=None)
+
+key_train = master_key.loc[master_key.iloc[:,10]==1]
+key_test = master_key.loc[master_key.iloc[:,10]==2]
+key_val = master_key.loc[master_key.iloc[:,10]==3]
+
+key_train.to_csv('key_train.csv',index=False,header=False,mode='w')
+key_test.to_csv('key_test.csv',index=False,header=False,mode='w')
+key_val.to_csv('key_val.csv',index=False,header=False,mode='w')
+            # Can use this below if I don't want to save new segments every time
+            # # if the segment doesn't exist already, save the pumping data to a csv in the Segments folder and to the master_key
+            # if master_exists:
+                
+            # if not old_master_key.iloc[:,0].eq(user_ID_string).any():
+            #     print('Entered IF')
+
     
