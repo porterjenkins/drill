fda0342ddcdc13f5cfb59d12b7ae49b5013d633e
Bryan Stringham <bryan.stringham@gmail.com>
2022-08-25 18:00:20 -0600

    Includes all up to date labeled and unlabeled segments. Should be totally ready to go and be used in the DL training and deployment.

--- Preprocessing/preprocessing_bs.py
+++ Preprocessing/preprocessing_bs.py
@@ -21,6 +21,7 @@ test = 0.1
 val = 0.1
 test_increment = train+test
 
+save_segments = 0
 
 master_exists = exists('master_key.csv')
 if master_exists:
@@ -29,7 +30,8 @@ if master_exists:
 
 # %%
 number_of_dates = 25
-dates_to_run = range(number_of_dates)
+# dates_to_run = range(number_of_dates)
+dates_to_run = range(1)
 
 for z in dates_to_run:
     include_in_training_data = dates.iloc[z,25]
@@ -40,7 +42,6 @@ for z in dates_to_run:
         # open label data excel file
         label_dir = 'C:/Users/bryan/Documents/1 PhD/drill/Raw_Data/Data-2022-'+start_date+'_to_2022-'+end_date+'/label-data-2022-'+start_date+'_to_2022-'+end_date+'.xlsx'
         label_data = pd.read_excel(label_dir,usecols="A:L,O:P,R",skiprows=[1])
-        
         
         month = label_data.iloc[:,0].dt.month.values
         day = label_data.iloc[:,0].dt.day.values
@@ -108,28 +109,30 @@ for z in dates_to_run:
                 #                                    C0              C1        C2        C3      C4              C5              C6              C7           C8               C9            C10
                 user_info = np.expand_dims(np.array([user_ID_string, label[j], month[j], day[j], date_info[j,0], date_info[j,1], date_info[j,2], duration[j], time_between[j], num_users[j], split_category]),axis=0)
                 user_info_df = pd.DataFrame(user_info)
-                if j == 0 and z==dates_to_run[0]:
-                    user_info_df.to_csv('master_key.csv',index=False,header=False,mode='w')
-                else:
-                    user_info_df.to_csv('master_key.csv',mode='a',index=False,header=False)
+                if save_segments:
+                    if j == 0 and z==0:
+                        user_info_df.to_csv('master_key.csv',index=False,header=False,mode='w')
+                    else:
+                        user_info_df.to_csv('master_key.csv',mode='a',index=False,header=False)
                 # extract dt, HE1, HE2, and HE3 for that particular user
                 sub_array = []
                 sub_array = sensor_data[start_times_i[j]:stop_times_i[j],:]
                 user_df = pd.DataFrame(sub_array)    
                 
                 string = 'Segments/'+user_ID_string+'.csv'
-                user_df.to_csv(string,index=False,header=False)
+                if save_segments:
+                    user_df.to_csv(string,index=False,header=False)
 
-# Create train/test/val keys from master key
-master_key = pd.read_csv('master_key.csv',header=None)
+if save_segments:
+    # Create train/test/val keys from master key
+    master_key = pd.read_csv('master_key.csv',header=None)
+    key_train = master_key.loc[master_key.iloc[:,10]==1]
+    key_test = master_key.loc[master_key.iloc[:,10]==2]
+    key_val = master_key.loc[master_key.iloc[:,10]==3]
+    key_train.to_csv('key_train.csv',index=False,header=False,mode='w')
+    key_test.to_csv('key_test.csv',index=False,header=False,mode='w')
+    key_val.to_csv('key_val.csv',index=False,header=False,mode='w')
 
-key_train = master_key.loc[master_key.iloc[:,10]==1]
-key_test = master_key.loc[master_key.iloc[:,10]==2]
-key_val = master_key.loc[master_key.iloc[:,10]==3]
-
-key_train.to_csv('key_train.csv',index=False,header=False,mode='w')
-key_test.to_csv('key_test.csv',index=False,header=False,mode='w')
-key_val.to_csv('key_val.csv',index=False,header=False,mode='w')
             # Can use this below if I don't want to save new segments every time
             # # if the segment doesn't exist already, save the pumping data to a csv in the Segments folder and to the master_key
             # if master_exists:
